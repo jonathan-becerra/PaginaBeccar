0c36cf6fd4f738cb8c93da4274b254a60513348f
Brayan Martin Becerra Andrade <Brayan.Becerra@beccar.local>
2023-03-23 12:23:08 -0600

    repositorio inicial

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//SWIPER SLIDER
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+  });
+  var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+      swiper: swiper,
+    },
+});
+
+
+//NAVIGATION BAR EFECTS ON SCROLL
+
+
+
+
+//RESPONSIVE NAVIGATION MENU TOGGLE
